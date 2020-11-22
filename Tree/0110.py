5dc57149467d50134ed58e33e52882a189bae6b2
lengain <lengain@qq.com>
2020-11-22 22:37:01 +0800

    add 0110

---
+++ Tree/0110.py
@@ -0,0 +1,19 @@
+"""
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return True
